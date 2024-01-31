454c0d41b8c8ff44c13211e14deeb42b20e84946
RafaelFrire <rafaelfesousa@hotmail.com>
2024-01-31 11:51:29 -0300

    add page to upload image

--- routers/upload.js
+++ routers/upload.js
@@ -1,21 +1,27 @@
 const express = require('express')
 const multer = require('multer')
 const router = express.Router()
+const path = require('path')
 const configMulter = require('../config/UploadConfig')
 
+const formPath = path.resolve(__dirname, '../view/form.html')
 
 const uploadImage = multer({storage:configMulter})
 
 
-router.post('/photos/upload', uploadImage.single('file'), (req, res) =>{
-    res.status(200).send('SUCESSO!!!')
-    res.end()
+router.post('/photos/upload', uploadImage.single('photo'), (req, res) =>{
+    res.sendFile(formPath)
+
 })
 
-router.get('/photos/upload', (req, res) =>{
-    fs.readFile(formUpload)
+router.get('/photos', (req, res) =>{
+    res.send("sucesso")
     res.end()
+ 
 })
+
+
+
 
 
 module.exports = router
